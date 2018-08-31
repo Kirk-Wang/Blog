45023750ae76a018827ffe4c0801eed19e0b04d8
Kirk.Wang <kirk.w.wang@gmail.com>
2018-08-31 21:07:30 +0800

    更新demo

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -7,7 +7,8 @@ import { connect } from "react-redux";
 import compose from "recompose/compose";
 import SettingsIcon from "@material-ui/icons/Settings";
 import LabelIcon from "@material-ui/icons/Label";
-import { translate, DashboardMenuItem, MenuItemLink, Responsive } from "react-admin";
+import MenuItem from "@material-ui/core/MenuItem";
+import { translate, MenuItemLink, Responsive } from "react-admin";
 import { withRouter } from "react-router-dom";
 
 import { CustomerIcon } from "./customers";
@@ -36,7 +37,15 @@ const styles: any = {
 
 const Comp = ({ onMenuClick, translate: trans, logout }: any) => (
     <div style={styles.main}>
-        <DashboardMenuItem onClick={onMenuClick} />
+        {/* <DashboardMenuItem onClick={onMenuClick} /> */}
+        {/*<a href="https://github.com/Kirk-Wang/react-admin-app" target="_blank">
+            Github
+        </a>*/}
+        <MenuItem>
+            <a href="https://github.com/Kirk-Wang/react-admin-app" target="_blank">
+                Github
+            </a>
+        </MenuItem>
         {items.map(item => (
             <MenuItemLink
                 key={item.name}
