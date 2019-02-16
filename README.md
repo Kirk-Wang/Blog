06aeb7751710524ee3d0ef0d02f29ced904b282f
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-16 18:16:42 +0800

    node.js promisify

--- README.md
+++ README.md
@@ -481,10 +481,29 @@ for(let i = 0; i < buf.length; i+=5) {
   </ul>
 </details>
 
-#### promisify
+#### promisify & async
 
 ```js
-
+const fs = require("fs");
+const path = require("path");
+const promisify = require("util").promisify;
+const read = promisify(fs.readFile);
+// read(path.join(__dirname,'./promisify.js'))
+// .then( data => {
+//   console.log(data.toString());
+// })
+// .catch(ex => {
+//   console.log(ex)
+// })
+async function test() {
+  try {
+    const content = await read(path.join(__dirname, "./promisify.js"));
+    console.log(content.toString());
+  } catch (ex) {
+    console.log(ex);
+  }
+}
+test();
 ```
 ---
 
--- nodejs-demo/fs/promisify.js
+++ nodejs-demo/fs/promisify.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
-const promisify = require('util').promisify
+const fs = require("fs");
+const path = require("path");
+const promisify = require("util").promisify;
 
-const read = promisify(fs.readFile)
+const read = promisify(fs.readFile);
 
 // read(path.join(__dirname,'./promisify.js'))
 // .then( data => {
@@ -14,8 +14,8 @@ const read = promisify(fs.readFile)
 
 async function test() {
   try {
-    const content = await read(path.join(__dirname,'./promisify.js'))
-    console.log(content.toString())
+    const content = await read(path.join(__dirname, "./promisify.js"));
+    console.log(content.toString());
   } catch (ex) {
     console.log(ex);
   }
