296d0ae2971f558912937d4dc173b96cdd2896d5
Kirk.Wang <kirk.w.wang@gmail.com>
2019-02-16 18:02:41 +0800

    node.js fs

--- nodejs-demo/fs/writestream.js
+++ nodejs-demo/fs/writestream.js
@@ -2,6 +2,18 @@ const fs = require('fs')
 const path = require('path')
 
 // stream --> 数据 & 方向
-const rs = fs.createReadStream(path.join(__dirname,'./readstream.js'))
+const ws = fs.createWriteStream(path.join(__dirname,'./test.txt'))
 
-rs.pipe(process.stdout) // 控制台
+const tid = setInterval(() => {
+  const num = parseInt(Math.random() * 10);
+  if (num < 9) {
+    ws.write(num+'')
+  } else {
+    clearInterval(tid)
+    ws.end() // 这里就会触发 finish 事件
+  }
+}, 200)
+
+ws.on('finish',  () => {
+  console.log('done!');
+})
