61c9858e4d9ceb640ca20c5abcadea46a3807a72
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-12 19:26:25 +0800

    update README.md

--- docs-demo/2-tutorial/my-app/src/App.tsx
+++ docs-demo/2-tutorial/my-app/src/App.tsx
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
