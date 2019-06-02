1741fd6535f07377cb071065733b08281e0638a6
Kirk.Wang <kirk.w.wang@gmail.com>
2019-06-02 23:40:01 +0800

    update demo

--- little-saga/building-your-own-redux-saga/1.5/1.js
+++ little-saga/building-your-own-redux-saga/1.5/1.js
@@ -7,6 +7,8 @@ function* gen() {
   console.assert(a === 1)
   const b = yield ['delay', 500]
   console.assert(b === '500ms elapsed')
+  const c = yield ['ping']
+  console.assert(c === "pong")
   console.log('exit……')
 }
 
@@ -29,6 +31,9 @@ function next(args, isErr) {
   } 
   else if(value[0] === 'delay') {
     setTimeout(() => next('500ms elapsed'), value[1])
+  }
+  else if(value[0] === 'ping') {
+    next('pong')
   }
   else {
     next(new Error('无法识别的 Effect'), true)
