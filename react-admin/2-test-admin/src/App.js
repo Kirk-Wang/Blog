89614f42feed0d08721282909a9918f136359d2b
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-07 18:33:05 +0800

    update

--- react-admin/2-test-admin/src/App.js
+++ react-admin/2-test-admin/src/App.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Admin } from "react-admin"
+import jsonServerProvider from 'ra-data-json-server'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
+const App = () => <Admin dataProvider={dataProvider} />
 
-export default App;
+export default App
