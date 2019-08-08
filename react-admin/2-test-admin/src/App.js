590713cf31bcd07b04c9a0380073b7accf2bb091
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-08 10:09:28 +0800

    update

--- react-admin/2-test-admin/src/App.js
+++ react-admin/2-test-admin/src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server'
+// import jsonServerProvider from 'ra-data-json-server'
 import { UserList } from './users';
 import { PostList, PostEdit, PostCreate } from './posts';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
+import jsonServerProvider from './dataProvider';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
