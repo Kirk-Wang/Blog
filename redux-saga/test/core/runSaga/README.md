b3ab4e4a0cb6c212a06bba801ccc9a561d67613e
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-21 01:04:37 +0800

    update

--- redux-saga/test/core/runSaga/README.md
+++ redux-saga/test/core/runSaga/README.md
@@ -1,13 +1,61 @@
 ### runSaga(...)
 
 1. 设计状态
-```
+```js
 state=0
 ```
 2. 设计 Action
-```
-{type: 'INCREMENT'}
-{type: 'DECREMENT'}
+```js
+{type: 'INCREMENT', payload: {step}}
+{type: 'DECREMENT', payload: {step}}
 {type: 'INCREMENT_ASYNC'}
 {type: 'DECREMENT_ASYNC'}
+```
+3. 设计 Reducer
+```js
+function reducer(state = 0, action) {
+  const { type, payload } = action
+  switch (type) {
+    case 'INCREMENT': {
+      return state + payload.step
+    } break;
+    case 'DECREMENT': {
+      return state - payload.step
+    } break;
+    default:
+      return state;
+  }
+}
+```
+4. 设计 Store 来管理状态
+```js
+function createStore(reducer) {
+  let state, listener
+  const dispatch = action => {
+    nextState = reducer(state, action)
+    if (nextState !== state && listener) {
+      state = nextState
+      listener()
+    }
+    return action
+  }
+  const getState = () => state
+  const subscribe = cb => listener = cb
+  dispatch({ type: 'INIT' })
+  return { dispatch, getState, subscribe }
+}
+```
+5. 配置 store，连接到 UI
+```js
+function configStore() {
+  const render = (input$ => 
+    state => input$.value = state
+  )(document.querySelector('input'))
+  const store = createStore(reducer)
+  store.subscribe(() => {
+    render(store.getState())
+  })
+  store.dispatch({type:'UNKNOWN'})
+  return store
+}
 ```
---
+++ redux-saga/test/core/runSaga/index.js
@@ -0,0 +1,94 @@
+
+function reducer(state = 0, action) {
+  const { type, payload } = action
+  switch (type) {
+    case 'INCREMENT': {
+      return state + payload.step
+    } break;
+    case 'DECREMENT': {
+      return state - payload.step
+    } break;
+    default:
+      return state;
+  }
+}
+
+function createStore(reducer) {
+  let state, listener
+  const dispatch = action => {
+    nextState = reducer(state, action)
+    if (nextState !== state && listener) {
+      state = nextState
+      listener()
+    }
+    return action
+  }
+  const getState = () => state
+  const subscribe = cb => listener = cb
+  dispatch({ type: 'INIT' })
+  return { dispatch, getState, subscribe }
+}
+
+function configStore() {
+  const render = (input$ => 
+    state => input$.value = state
+  )(document.querySelector('input'))
+  const store = createStore(reducer)
+  store.subscribe(() => {
+    render(store.getState())
+  })
+  store.dispatch({type:'UNKNOWN'})
+  return store
+}
+
+function* root() {
+  const { all, fork} = ReduxSagaEffects
+  yield all([fork(incAsync), fork(decAsync)])
+}
+
+function* incAsync() {
+  const { take, delay, put} = ReduxSagaEffects
+  while(true) {
+    yield take('INCREMENT_ASYNC')
+    yield delay(500)
+    yield put({ type: 'INCREMENT', payload: { step: 3 } })
+  }
+}
+
+function* decAsync() {
+  const { take, delay, put} = ReduxSagaEffects
+  while(true) {
+    yield take('DECREMENT_ASYNC')
+    yield delay(1000)
+    yield put({ type: 'DECREMENT', payload: { step: 3 }  })
+  }
+}
+
+let { dispatch, getState } = configStore()
+const channel = ReduxSaga.stdChannel()
+const IO = {
+  channel,
+  dispatch,
+  getState
+}
+
+const enhancer = next => action => {
+  // hit reducers
+ result = next(action)
+ channel.put(action)
+ return result
+}
+dispatch = enhancer(dispatch)
+
+task = ReduxSaga.runSaga(IO, root)
+// bind events
+document.addEventListener('click', (e) => {
+  switch (e.target.id) {
+    case 'inc': {
+      dispatch({type: 'INCREMENT_ASYNC'})
+    } break;
+    case 'dec': {
+      dispatch({type: 'DECREMENT_ASYNC'})
+    } break;
+  }
+})
