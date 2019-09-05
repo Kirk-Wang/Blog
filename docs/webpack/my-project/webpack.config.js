72203a90747aa7f99955121ea3e730077fc8dd4f
Kirk.Wang <kirk.w.wang@gmail.com>
2019-09-05 23:46:08 +0800

    update

--- docs/webpack/my-project/webpack.config.js
+++ docs/webpack/my-project/webpack.config.js
@@ -3,7 +3,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
