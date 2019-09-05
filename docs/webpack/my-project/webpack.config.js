be6dd1217c848f443eed1e7db0bb42fca2a9413a
Kirk.Wang <kirk.w.wang@gmail.com>
2019-09-05 23:46:39 +0800

    update

--- docs/webpack/my-project/webpack.config.js
+++ docs/webpack/my-project/webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     search: './src/search.js'
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
   },
   mode: 'production'
 }
