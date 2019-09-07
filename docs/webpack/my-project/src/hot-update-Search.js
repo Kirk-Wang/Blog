4a1452a6e4be04894027fd28ba08ed5ce933394e
Kirk.Wang <kirk.w.wang@gmail.com>
2019-09-07 16:16:36 +0800

    update

--- docs/webpack/my-project/src/hot-update-Search.js
+++ docs/webpack/my-project/src/hot-update-Search.js
@@ -4,6 +4,6 @@ import logo from './images/logo.png'
 
 export class Search extends React.Component {
   render() {
-    return <div className="search-text">搜索文字内容 <img src={logo} /> </div>
+    return <div className="search-text">搜索文字内容: <img src={logo} /> </div>
   }
 }
