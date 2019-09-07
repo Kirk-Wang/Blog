c7bfcf9bc9022a9294e3d4e17fba244c4084de97
Kirk.Wang <kirk.w.wang@gmail.com>
2019-09-07 13:05:44 +0800

    update

--- docs/webpack/my-project/src/search.js
+++ docs/webpack/my-project/src/search.js
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './search.css'
 
 class Search extends React.Component {
   render() {
-    return <div>search text</div>
+    return <div className="search-text">search text</div>
   }
 }
 
