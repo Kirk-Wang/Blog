5d571f58651b2419fcf4a05f8865f7b49cc04cd9
Kirk.Wang <kirk.w.wang@gmail.com>
2019-11-06 23:36:28 +0800

    update

--- Isomorphic/demo/src/home/Controller.js
+++ Isomorphic/demo/src/home/Controller.js
@@ -1,12 +1,7 @@
-import Controller from 'react-imvc/controller' // 加载 react-imvc controller 控制器
+import Controller from 'react-imvc/controller'
 import React from 'react'
+import View from './View'
 
-export default class Home extends Controller { // 继承它，编写你的控制器逻辑
-    View = View // 将 react 组件赋值给控制器的 View 属性
-}
-
-function View() {
-    return (
-        <h1>Hello React-IMVC</h1>
-    )
+export default class Home extends Controller {
+    View = View
 }
