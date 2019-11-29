40d78c98b1c391ed92fac0699eccbfd4ff78ec54
Kirk.Wang <kirk.w.wang@gmail.com>
2019-11-29 11:38:56 +0800

    update

--- react-native/expo-demo/App.js
+++ react-native/expo-demo/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your appxx!</Text>
+      <Text>I think that I must do it~!</Text>
     </View>
   );
 }
